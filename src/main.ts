3ec383e82fbb0ca6a39b824aadf75fec7c007a66
Fuxing Loh <git@fuxing.dev>
2021-01-15 16:28:07 +0800

    feat: set appended labels to output

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import * as core from '@actions/core'
 const githubToken = core.getInput('github-token')
 const configPath = core.getInput('config-path', {required: true})
 
-run(githubToken, configPath).catch(error => {
-  core.error(error)
-  core.setFailed(error.message)
-})
+run(githubToken, configPath)
+  .then(value => {
+    core.setOutput('labels', value)
+  })
+  .catch(error => {
+    core.error(error)
+    core.setFailed(error.message)
+  })
